71051934e062346025415d12b03217a0135784e7
FedenkoM <fedenkomisha@gmail.com>
2021-09-15 17:29:10 +0300

    first commit

---
+++ src/main/java/com/example/dockerdeploydemo/DockerDeployDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.dockerdeploydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerDeployDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerDeployDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dockerdeploydemo/DockerDeployDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dockerdeploydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerDeployDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
