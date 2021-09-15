88f689fe68f4275f508d39203df4f464668727c6
FedenkoM <fedenkomisha@gmail.com>
2021-09-15 19:04:58 +0300

    add deploy job to workflow

---
+++ src/test/java/com/example/dockerdeploydemo/controller/HomeControllerTest.java
@@ -0,0 +1,19 @@
+package com.example.dockerdeploydemo.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class HomeControllerTest {
+
+    @Autowired
+    private HomeController homeController;
+
+    @Test
+    void hello() {
+        assertEquals("Hello from docker deploy!", homeController.hello() );
+    }
+}
