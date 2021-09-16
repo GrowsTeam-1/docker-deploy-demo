4eb7fa1cb675cd56f2e5dc779bd101ec1dfd4801
FedenkoM <fedenkomisha@gmail.com>
2021-09-16 13:56:39 +0300

    send secret env to image

--- src/main/java/com/example/dockerdeploydemo/controller/HomeController.java
+++ src/main/java/com/example/dockerdeploydemo/controller/HomeController.java
@@ -1,14 +1,23 @@
 package com.example.dockerdeploydemo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
+    @Value("${app.value}")
+    private String secretValue;
+
     @GetMapping
     public String hello() {
         return "Hello from docker deploy! Now it running ot elasticbeans";
+    }
+
+    @GetMapping("/secret")
+    public String getSecretValue() {
+        return secretValue;
     }
 
 }
